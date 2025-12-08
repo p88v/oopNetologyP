7811554a5a639d449befbfca578c01a155bf63be
p88 <ereschko.m@yandex.ru>
2025-12-08 12:16:18 +0300

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,40 @@
+public class Book {
+
+    public String title;
+    public int releaseYear;
+    public String author;
+    public int pages;
+
+
+    public Book(String title, int releaseYear, String author, int pages) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public boolean isBig(){
+        if (pages > 500){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean matches(String word){
+        if (word.equals(title) && word.equals(author)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public int estimatePrice(){
+        if (pages*3<=250){
+            return 250;
+        }else {
+            return pages*3;
+        }
+    }
+
+}
